aeaf5d3feb700e447a3926044c3e7642b2fd152d
Tomasz Cac <tomaszandrzej1234@gmail.com>
2024-11-18 16:29:17 +0100

    Added first implementation of models and EF packages

---
+++ TaskManagmentSystemApiProject/TaskManagmentSystemApiProject/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskManagmentSystemApiProject.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public int Task { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public string Text { get; set; } = "";
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ TaskManagmentSystemApiProject/TaskManagmentSystemApiProject/Models/Task.cs
@@ -0,0 +1,25 @@
+﻿namespace TaskManagmentSystemApiProject.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = "";
+
+        public string Description { get; set; } = "";
+
+        public int CreatedBy {  get; set; }
+
+        public int AssingedTo { get; set; }
+
+        public enum Priority { Low, Medium, High }
+
+        public enum Status { Pending, InProgress, Completed }
+
+        public DateTime DueDate {  get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
