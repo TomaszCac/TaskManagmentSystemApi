864b5aabb57644aa7a010ab4d9509f0614a55e01
Tomasz Cac <tomaszandrzej1234@gmail.com>
2024-11-18 17:37:45 +0100

    Added cors policy for different environments

--- TaskManagmentSystemApiProject/TaskManagmentSystemApiProject/Program.cs
+++ TaskManagmentSystemApiProject/TaskManagmentSystemApiProject/Program.cs
@@ -4,7 +4,36 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors((options) =>
+{
+    options.AddPolicy("DevCors", (corsBuilder) =>
+    {
+        corsBuilder.WithOrigins("http://localhost:4200", "http://localhost:3000", "http://localhost:8000")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials();
+    });
+    options.AddPolicy("ProdCors", (corsBuilder) =>
+    {
+        corsBuilder.WithOrigins("https://myproductionsite.com")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials();
+    });
+});
+
+
 var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors("DevCors");
+}
+else
+{
+    app.UseCors("ProdCors");
+}
 
 // Configure the HTTP request pipeline.
 
